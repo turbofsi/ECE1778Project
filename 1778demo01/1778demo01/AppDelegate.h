6209efb9451542d6487932c43172a8526bb8f2af
lxy0425 <lvxinyun@lvxinyundeMacBook-Pro.local>
2015-01-17 21:36:01 -0500

    First Public of Three of my assignment

---
+++ 1778demo01/1778demo01/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  1778demo01
+//
+//  Created by  吕欣韵 on 2015-01-13.
+//  Copyright (c) 2015 UofT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 1778demo01/1778demo01/ViewController.h
@@ -0,0 +1,27 @@
+//
+//  ViewController.h
+//  1778demo01
+//
+//  Created by  吕欣韵 on 2015-01-13.
+//  Copyright (c) 2015 UofT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <AVFoundation/AVFoundation.h>
+#import <CoreVideo/CoreVideo.h>
+#import <CoreMedia/CoreMedia.h>
+#import <ImageIO/ImageIO.h>
+#import <QuartzCore/QuartzCore.h>
+#import <AudioToolbox/AudioToolbox.h>
+
+@interface ViewController : UIViewController<AVCaptureVideoDataOutputSampleBufferDelegate>
+
+@property (weak, nonatomic) IBOutlet UIImageView *liveImage;
+@property (nonatomic, strong) AVCaptureVideoDataOutput *videoDataOutput;
+@property (nonatomic, strong) AVCaptureVideoPreviewLayer *previewLayer;
+@property (nonatomic, strong) CIDetector *faceDetector;
+@property (nonatomic) dispatch_queue_t videoDataOutputQueue;
+
+
+@end
+
